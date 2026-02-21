2885d14e5e4b146bc95f10b8d50f97acfa2ecb28
Miral8484-ce <miral31122004@gmail.com>
2026-02-21 21:47:32 +0530

    Add files via upload

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,16 @@
+
+#Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+Addition=num1+num2
+Subtraction=num1-num2
+Multiplication=num1*num2
+Division=num1/num2
+print(" ")
+print("Addition: ",Addition)
+print("Subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",Division)
---
+++ Assignment_1_Task_2.py
@@ -0,0 +1,9 @@
+#Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+#2.  Concatenates the first name and last name into a full name.
+#3.  Prints a personalized greeting message using the full name.
+
+First_Name=input("Enter your first name: ")
+Last_Name=input("Enter your Last name: ")
+print(" ")
+print(f"Hello,{First_Name} {Last_Name}! welcome to the Python program")
